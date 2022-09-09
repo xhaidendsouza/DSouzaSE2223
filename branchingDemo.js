d0aa5817d706d7accb4a19a834784e09f74e5f73
xhaidendsouza <69084603+xhaidendsouza@users.noreply.github.com>
2022-09-09 12:40:03 -0400

    Update branchingDemo.js
    1. Added functions to get the day and year
    2. Reformatted the output
    3. Reformatted the code for the list

--- branchingDemo.js
+++ branchingDemo.js
@@ -2,13 +2,21 @@
 // Author:      X. D'Souza
 // Objective:   Demonstrate how to use Github branches
 
-const months = ["January", "February", "March", 
-                "April", "May", "June", "July", 
-                "August", "September", "October", 
-                "November", "December"];
+const months = ["January", "February", "March",  "April", 
+                "May", "June", "July",  "August", "September",
+                "October", "November", "December"];
 
 const d = new Date();
 
-let month = months[d.getMonth()];
+// getMonth() returns the code as an integer (0-11)
+// getDay() returns the day as an integer (1-31)
+// getFullYear() returns the year as a 4 digit number
 
-console.log("The date is: " + d);
+let month = months[d.getMonth()];
+let day = d.getDay();
+let year = d.getFullYear();
+
+let currentDate = "The current date is: " + month + " " + day + " " + year
+
+console.log(d);
+console.log(currentDate)
